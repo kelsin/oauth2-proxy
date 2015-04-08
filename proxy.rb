af1affd50c603bf3393c7190164f0ba4042f960a
Christopher Giroir <kelsin@valefor.com>
2015-04-07 20:40:54 -0700

    Adding omniauth

--- proxy.rb
+++ proxy.rb
@@ -1,4 +1,26 @@
 require 'sinatra'
+require 'omniauth'
+require 'omniauth-oauth2'
+
+# These variables should all be set in your environment
+CLIENT_ID = ENV['CLIENT_ID']
+CLIENT_SECRET = ENV['CLIENT_SECRET']
+REDIRECT_URL = ENV['REDIRECT_URL']
+AUTHORIZE_URL = ENV['AUTHORIZE_URL']
+TOKEN_URL = ENV['TOKEN_URL']
+SCOPE = ENV['SCOPE']
+
+use Rack::Session::Cookie
+use OmniAuth::Builder do
+  provider :oauth2, CLIENT_ID, CLIENT_SECRET, :client_options => {
+             :authorize_url => AUTHORIZE_URL,
+             :token_url => TOKEN_URL,
+             :scope => SCOPE }
+end
+
+post '/auth/:name/callback' do
+  request.env['omniauth.auth'].inspect
+end
 
 get '/' do
   "Hello World!"
